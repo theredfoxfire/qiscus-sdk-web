7aced2730b511991805e418c86a01073b5603612
Rijalul Fikri <fikri.desertlion@gmail.com>
2017-06-19 19:16:07 +0700

    Merging from feature/reply, prepare for building

--- src/vue/main.js
+++ src/vue/main.js
@@ -9,15 +9,15 @@ Vue.use(Toasted, {
 })
 
 // QiscusSDK.mode = 'wide';
-QiscusSDK.init({
-  AppId: 'dragongo',
-  options: {
-    headerClickedCallback(response) {
-      console.info('header di click', response);
-    }
-  }
-});
-QiscusSDK.setUser('fikri@qiscus.com', 'password', 'fikri');
+// QiscusSDK.init({
+//   AppId: 'dragongo',
+//   options: {
+//     headerClickedCallback(response) {
+//       console.info('header di click', response);
+//     }
+//   }
+// });
+// QiscusSDK.setUser('fikri@qiscus.com', 'password', 'fikri');
 
 /* let's render the view layer */
 window.vm = new Vue({
